6cdac986ab02a2d43d0a414ad60b3c098f0871dc
Luis Vazquez <luisc.vazquezb@gmail.com>
2025-06-11 19:30:49 -0600

    comentamos sectores, produccion

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from config import configure_app
 # Importar los blueprints
 from routes.auth import auth_bp
 from routes.clientes import clientes_bp
-from routes.usuarios import usuarios_bp
-from routes.sectores import sectores_bp
-from routes.produccion import produccion_bp
+# from routes.usuarios import usuarios_bp
+# from routes.sectores import sectores_bp
+#from routes.produccion import produccion_bp
 
 app = Flask(__name__)
 configure_app(app)
